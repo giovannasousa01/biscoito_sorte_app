fefe3896f4b9f526e99a2a27cedaf57b72769bfd
Giovanna Sousa <giovannasousa.code@gmail.com>
2022-12-26 16:15:06 -0400

    Primeiro commit / First commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Biscoito da Sorte',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+
+      ),
+      home: const HomePage(),
+    );
+  }
+}
